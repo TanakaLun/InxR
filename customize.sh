f1d48b37b4bbae7c30c0fa594e573538af69cb25
TanakaLun <2709410138@qq.com>
2026-02-12 17:36:32 +0800

    refactor: 改进service中的缓存清理逻辑

--- customize.sh
+++ customize.sh
@@ -1,2 +1,3 @@
 #!/system/bin/sh
 set_perm_recursive "$MODDIR/system" 0 0 755 644
+touch "$MODDIR/inxed"
--- service.sh
+++ service.sh
@@ -1,2 +1,8 @@
 #!/system/bin/sh
-rm -r /data/system/package_cache/*
+MODPATH="/data/adb/modules/installerx_revived"
+if [ ! -f "$MODPATH/inxed" ]; then
+    rm -r /data/system/package_cache/*
+else
+    # 若希望“每次更新都清理”，可以取消注释掉下面这一行
+    # rm "$MODPATH/inxed"
+fi
